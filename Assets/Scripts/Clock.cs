05b52d59a2706965e5efda5a074a9aa1f407dc34
Juliane Fink <jule-fink@gmx.de>
2018-11-04 18:25:13 +0100

    add material and update time on clock

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class Clock : MonoBehaviour {
+
+    TextMeshPro timeDisplay;
+    
+	void Start () {
+        timeDisplay = GetComponentInChildren<TextMeshPro>();
+	}
+	
+	void Update () {
+        String time = DateTime.Now.ToString("HH:mm");
+        timeDisplay.text = time;
+
+    }
+}
