87b8aa46cc9d98e55d92873380f89d67b5802067
Juliane Fink <jule-fink@gmx.de>
2018-11-04 21:31:36 +0100

    add simple date display

---
+++ Assets/Scripts/DayCalendar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DayCalendar : MonoBehaviour {
+
+    [SerializeField]
+    TextMeshPro dayDisplay;
+
+    [SerializeField]
+    TextMeshPro monthYearDisplay;
+
+    int day = 0;
+
+	void Update () {
+        int newDay = DateTime.Now.Day;
+        if (day != newDay) {
+            dayDisplay.text = DateTime.Now.ToString("dd");
+            monthYearDisplay.text = DateTime.Now.ToString("mm/yy");
+        }
+		
+	}
+}
